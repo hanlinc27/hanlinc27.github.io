3f04c5cf343907185af96688fd72d42ac15dd3b9
Hanlin Cheng <hanlin.cheng@uwaterloo.ca>
2020-05-08 23:48:04 -0400

    finished the about page layout

---
+++ src/components/about-me/about-avatar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import logo from './logo.png'; // Tell webpack this JS file uses this image
+ // /logo.84287d09.png
+function AboutPicture() {
+  // Import result is the URL of your image
+  return <img src={"https://github.com/hanlinc27/hanlinc27.github.io/blob/master/static/about-picture.png?raw=true"} alt="Avatar " />;
+}
+export default AboutPicture;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,20 +4,35 @@ import { BlockText } from 'components/block-text/BlockText';
 import { Highlight } from "components/intro/Highlight";
 import { Intro } from 'components/intro/Intro';
 import 'styles/fonts.css';
-
+import Image from 'react-bootstrap/Image';
 // tslint:disable no-default-export
 export default () => (
   <>
     <Helmet title="about me" />
 
     <Intro>
-      Still waiting for my hogwarts letter...
+    <div className = "landing-heading">  Still waiting for my Hogwarts letter... </div>
       <div className = "about-body">
-    <img className = "about-body__image" src="https://github.com/hanlinc27/hanlinc27.github.io/blob/master/static/about-picture.png?raw=true" alt="profilepic"></img>
+   
     {/* <div className="about-body__text">
    check text
 </div> */}
-<p>che<img className = "about-body__image" src="https://github.com/hanlinc27/hanlinc27.github.io/blob/master/static/about-picture.png?raw=true" className = "photo" alt="profilepic"></img></p>
+
+<Image className = "about-picture" src="https://github.com/hanlinc27/hanlinc27.github.io/blob/master/static/about-picture.png?raw=true"  />
+<div className = "about-text">
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+<br></br>
+<br></br>
+Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+<br></br>
+<br></br>
+In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+<br></br>
+<br></br>
+Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus,
+</div>
+
+{/* <p>che<img className = "about-body__image" src="https://github.com/hanlinc27/hanlinc27.github.io/blob/master/static/about-picture.png?raw=true" class = "photo" alt="profilepic"></img></p> */}
 </div>
 
 {/* 
