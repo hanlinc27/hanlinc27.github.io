30f097c7ebba7e6f1490fcf6ba79f5b0bf05f908
Hanlin Cheng <h62cheng@uwaterloo.ca>
2023-09-14 14:17:05 -0400

    f23 updates

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 import { Intro } from 'components/intro/Intro';
 import { Container } from 'components/container/Container';
 import 'styles/fonts.css';
 import styled from 'styled-components';
 
-import mePhoto from "assets/images/about-photo.svg";
+import mePhoto from 'assets/images/about-photo.svg';
 // tslint:disable no-default-export
 
 const StyledPhoto = styled.img`
@@ -21,8 +21,7 @@ const StyledDiv = styled.div`
   font-size: 14px;
   line-height: 17px;
   margin-top: 12px;
-  color: #1E1A1D;
-
+  color: #1e1a1d;
 `;
 
 const StyledSubtitle = styled.div`
@@ -31,7 +30,7 @@ const StyledSubtitle = styled.div`
   font-weight: 300;
   font-size: 14px;
   line-height: 17px;
-  color: #426B69;
+  color: #426b69;
 `;
 
 const StyledSmallText = styled.div`
@@ -40,19 +39,18 @@ const StyledSmallText = styled.div`
   font-weight: 300;
   font-size: 12px;
   line-height: 14px;
-  color: #1E1A1D;
+  color: #1e1a1d;
   display: inline;
-
 `;
 
 const StyledBoldSmallText = styled.div`
-font-family: Rubik;
-font-style: normal;
-font-weight: 500;
-font-size: 12px;
-line-height: 14px;
-color: #1E1A1D;
-display: inline;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  color: #1e1a1d;
+  display: inline;
 `;
 
 const StyledContainer = styled.div`
@@ -71,13 +69,13 @@ const StyledLink = styled.a`
   font-size: 14px;
   line-height: 14px;
   text-decoration: underline;
-  color: #1E1A1D;
+  color: #1e1a1d;
 
-  &:hover{
-    color: #426B69;
+  &:hover {
+    color: #426b69;
     text-decoration: none !important;
   }
-  &:focus{
+  &:focus {
     text-decoration: none !important;
   }
 `;
@@ -90,36 +88,34 @@ const StyledSubtitleHeading = styled.div`
 
   letter-spacing: 0.05em;
 
-  color: #1E1A1D;
+  color: #1e1a1d;
   margin-top: 48px;
   padding-bottom: 8px;
-  border-bottom: 1px solid  #1E1A1D;;
+  border-bottom: 1px solid #1e1a1d;
 `;
 
 const ColouredDiv = styled.div`
-font-family: Rubik;
-font-style: normal;
-font-weight: 300;
-font-size: 14px;
-line-height: 17px;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 17px;
 
-color: #1E1A1D;
-margin-top: 28px;
-background-color:  #DFE3DF;
-padding: 8px;
-margin-bottom: 32px;
+  color: #1e1a1d;
+  margin-top: 28px;
+  background-color: #dfe3df;
+  padding: 8px;
+  margin-bottom: 32px;
 `;
 
 const StyledBullets = styled.div`
-
-font-family: Rubik;
-font-style: normal;
-font-weight: 300;
-font-size: 14px;
-line-height: 17px;
-margin-bottom: 8px;
-color: #1E1A1D;
-
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 17px;
+  margin-bottom: 8px;
+  color: #1e1a1d;
 `;
 export default () => (
   <>
@@ -127,46 +123,67 @@ export default () => (
       <Helmet title="about me" />
       <div className="row animated animatedFadeInUp fadeInUp">
         <Intro>
-
           <StyledSubtitle>Nice to Meet You! </StyledSubtitle>
           <div className="landing-heading"> As a child, I fell in love with jigsaw puzzles. </div>
 
           <StyledContainer>
-            <StyledDiv >
-              Somewhere along the way, I realized there’s not much difference between approaching puzzle pieces and lines of code. My curiosity lies in learning how to positively connect the code we write directly to empower the humans that use it. I’ve explored this by engaging with the           <StyledLink target="_blank" href="http://civictech.ca/">Civic Tech Toronto</StyledLink> community and at a social impact club,
-
-           <StyledLink target="_blank" href="http://uwblueprint.org">UW Blueprint</StyledLink>
-.
+            <StyledDiv>
+              Somewhere along the way, I realized there’s not much difference between approaching
+              puzzle pieces and lines of code. My curiosity lies in learning how to positively
+              connect the code we write directly to empower the humans that use it. I’ve explored
+              this by engaging with the{' '}
+              <StyledLink target="_blank" href="http://civictech.ca/">
+                Civic Tech Toronto
+              </StyledLink>{' '}
+              community and at a social impact club,
+              <StyledLink target="_blank" href="http://uwblueprint.org">
+                UW Blueprint
+              </StyledLink>
+              .<br></br>
+              <br></br>I also happen to be finishing my final year as a Systems Design Engineering
+              student at the University of Waterloo. Why Systems Design? Because humans are diversly
+              complex, so it’s exciting to understand the “big picture” story behind all the
+              different puzzles we’re trying to solve together in this world.
               <br></br>
               <br></br>
-
-          I also happen to be a second-year Systems  Design Engineering student at the University of Waterloo. Why Systems Design? Because humans are diversly complex, so it’s exciting to understand the “big picture” story behind all the different puzzles we’re trying to solve together in this world.
-          <br></br>
-          <br></br>
-          Moving forward, I hope to fit the little and large jigsaws we encounter to create intentionally delightful interactions.
-
+              Moving forward, I hope to fit the little and large jigsaws we encounter to create
+              intentionally delightful interactions.
               <br></br>
               <br></br>
               <StyledSmallText>Thank you for stopping by! I’m currently seeking </StyledSmallText>
-              <StyledBoldSmallText>a 4 month Software Development Internship</StyledBoldSmallText>
-              <StyledSmallText> - If you’d like to chat, I'm always open to listen and learn.</StyledSmallText>
-              {/* <StyledLink target="_blank" href="https://hanlincheng.me/static/hanlin_cheng_resume.pdf">here.</StyledLink> */}
-
+              <StyledBoldSmallText>
+                a new grad Software Engineering role for Spring 2024!
+              </StyledBoldSmallText>
+              <StyledSmallText>
+                {' '}
+                - If you’d like to chat, I'm always open to listen and learn.
+              </StyledSmallText>
+              <br />
+              <br />
+              <StyledLink
+                target="_blank"
+                href="https://hanlincheng.me/static/hanlin_cheng_resume.pdf"
+              >
+                Resume here.
+              </StyledLink>
             </StyledDiv>
-
           </StyledContainer>
           <StyledPhoto src={mePhoto}></StyledPhoto>
-          <StyledSubtitleHeading>What I'm Up To!</StyledSubtitleHeading>
-<ColouredDiv>In my free time, I enjoy debating over fantasy basketball (the Spurs!), shots of espresso, building puzzles, and watercolour painting.</ColouredDiv>
-       
-       <StyledBullets>• 📚 Finished my 2B school term. </StyledBullets>
-       <StyledBullets>• 🍪 Eating lots of peanut butter. </StyledBullets>
-       <StyledBullets>• 🎤 Published the eleventh episode of a podcast with friends.
-        <a target="_blank" href="https://anchor.fm/for-context"> Listen here!</a>
+          {/* <StyledSubtitleHeading>What I'm Up To!</StyledSubtitleHeading>
+          <ColouredDiv>
+            In my free time, I enjoy debating over fantasy basketball (the Spurs!), shots of
+            espresso, building puzzles, and watercolour painting.
+          </ColouredDiv>
 
-       </StyledBullets>
-
-
+          <StyledBullets>• 📚 Finished my 2B school term. </StyledBullets>
+          <StyledBullets>• 🍪 Eating lots of peanut butter. </StyledBullets>
+          <StyledBullets>
+            • 🎤 Published the eleventh episode of a podcast with friends.
+            <a target="_blank" href="https://anchor.fm/for-context">
+              {' '}
+              Listen here!
+            </a>
+          </StyledBullets> */}
         </Intro>
       </div>
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,41 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 import 'styles/fonts.css';
 import { Container } from 'components/container/Container';
 import { Intro } from 'components/intro/Intro';
 import styled from 'styled-components';
 import { Project } from 'components/project-blurb/Project';
-import { SpecialProject} from 'components/project-blurb/SpecialProject';
-import CopyMailTo from "react-copy-mailto";
+import { SpecialProject } from 'components/project-blurb/SpecialProject';
+import CopyMailTo from 'react-copy-mailto';
 
+import github from 'assets/images/github.svg';
+import linkedin from 'assets/images/linkedin.svg';
+import email from 'assets/images/email.svg';
 
-import  github from "assets/images/github.svg";
-import linkedin from "assets/images/linkedin.svg";
-import email from "assets/images/email.svg";
+import first from 'assets/images/first.svg';
+import second from 'assets/images/second.svg';
+import third from 'assets/images/third.svg';
+import fourth from 'assets/images/fourth.svg';
 
-import first from "assets/images/first.svg";
-import second from "assets/images/second.svg";
-import third from "assets/images/third.svg";
-import fourth from "assets/images/fourth.svg";
-
-import voiceflow from "assets/images/voiceflow.svg";
-import shoeImage from "assets/images/shoe-image.svg";
-import imaginecomms from "assets/images/imaginecomms.svg";
-import ctto from "assets/images/ctto.svg";
-import pivotal from "assets/images/pivotal.svg";
-import sunnybrook from "assets/images/sunnybrook.svg";
-import hackathons from "assets/images/hackathons.svg";
-import forcontext from "assets/images/forcontext.svg";
+import voiceflow from 'assets/images/voiceflow.svg';
+import shoeImage from 'assets/images/shoe-image.svg';
+import imaginecomms from 'assets/images/imaginecomms.svg';
+import ctto from 'assets/images/ctto.svg';
+import pivotal from 'assets/images/pivotal.svg';
+import sunnybrook from 'assets/images/sunnybrook.svg';
+import hackathons from 'assets/images/hackathons.svg';
+import forcontext from 'assets/images/forcontext.svg';
 // tslint:disable-next-line: no-default-export
 const StyledLink = styled.a`
   cursor: pointer;
   text-decoration: underline;
-  color: #1E1A1D;
+  color: #1e1a1d;
 
-  &:hover{
-    color: #426B69;
+  &:hover {
+    color: #426b69;
     text-decoration: none !important;
   }
-  &:focus{
+  &:focus {
     text-decoration: none !important;
   }
 `;
@@ -59,10 +58,10 @@ const StyledSubittle = styled.div`
 
   letter-spacing: 0.05em;
 
-  color: #1E1A1D;
+  color: #1e1a1d;
   margin-top: 48px;
   padding-bottom: 8px;
-  border-bottom: 1px solid  #1E1A1D;;
+  border-bottom: 1px solid #1e1a1d;
 `;
 
 const StyledProject = styled(Project)`
@@ -81,11 +80,11 @@ const StyledProjectImage = styled.img`
   align-items: right;
 `;
 
-const StyledProjectContainer = styled.div`  
+const StyledProjectContainer = styled.div`
   display: flex;
   flex-direction: row;
   @media only screen and (max-width: 800px) {
-  flex-wrap: wrap;
+    flex-wrap: wrap;
   }
   margin-top: 20px;
   margin-bottom: 0%;
@@ -94,7 +93,7 @@ const StyledProjectContainer = styled.div`
 const StyledIcon = styled.img`
   padding-left: 8px;
   width: 28px;
-  cursor:pointer;
+  cursor: pointer;
   display: inline;
   margin-right: 16px;
 `;
@@ -104,71 +103,100 @@ const StyledEmailIcon = styled.img`
   overflow: hidden;
   margin-bottom: 14px;
   width: 28px;
-  cursor:pointer;
+  cursor: pointer;
   display: inline;
   margin-right: 50px;
 `;
-
-
 
 export default () => (
   <>
     <Helmet title="Hanlin Cheng" />
     <Container>
       <Intro>
-
-        <div className="landing-heading animated animatedFadeInUp fadeInUp">  Hi there, I'm Hanlin. </div>
+        <div className="landing-heading animated animatedFadeInUp fadeInUp">
+          {' '}
+          Hi there, I'm Hanlin.{' '}
+        </div>
 
         <br></br>
 
-        <div className="landing-subtitle animated animatedFadeInUp fadeInUp"> I’m striving to create beautiful, intentional, and meaningful software interactions.</div>
+        <div className="landing-subtitle animated animatedFadeInUp fadeInUp">
+          {' '}
+          I’m striving to create beautiful, intentional, and meaningful software interactions.
+        </div>
         <div className="landing-body animated animatedFadeInUp fadeInUp">
-          I’m a third year engineering student at the University of Waterloo. I’ve previously interned at <StyledDiv></StyledDiv>
-           <StyledLink target="_blank" href="https://www.intuit.com/">Intuit
-</StyledLink>
-<StyledDiv>, </StyledDiv>
-          <StyledLink target="_blank" href="https://www.voiceflow.com/">Voiceflow
-</StyledLink>
-<StyledDiv>, </StyledDiv>
-          <StyledLink target="_blank" href="https://imaginecommunications.com/">Imagine Communications
-</StyledLink>
+          I’m a final year engineering student at the University of Waterloo. I’ve previously
+          interned at <StyledDiv></StyledDiv>
+          <StyledLink target="_blank" href="https://www.withpavilion.com/">
+            Pavilion
+          </StyledLink>
           <StyledDiv>, </StyledDiv>
-          <StyledLink target="_blank" href="https://tanzu.vmware.com/tanzu">VMware</StyledLink>
+          <StyledLink target="_blank" href="https://www.moderntreasury.com/">
+            Modern Treasury
+          </StyledLink>
+          <StyledDiv> , </StyledDiv>
+          <StyledLink target="_blank" href="https://www.intuit.com/">
+            Intuit
+          </StyledLink>
           <StyledDiv>, and </StyledDiv>
-          <StyledLink target="_blank" href="https://sunnybrook.ca/research/content/?page=sri-groups-fus">Sunnybrook Research Institute</StyledLink>
+          <StyledLink target="_blank" href="https://www.voiceflow.com/">
+            Voiceflow
+          </StyledLink>
           <StyledDiv>. Additionally, I volunteer with </StyledDiv>
-          <StyledLink target="_blank" href="https://uwblueprint.org/">UW Blueprint</StyledLink>
+          <StyledLink target="_blank" href="https://uwblueprint.org/">
+            UW Blueprint
+          </StyledLink>
           <StyledDiv> and </StyledDiv>
-          <StyledLink target="_blank" href="http://civictech.ca/">Civic Tech Toronto</StyledLink>
-          <StyledDiv> to build pro-bono software.  </StyledDiv>
+          <StyledLink target="_blank" href="http://civictech.ca/">
+            Civic Tech Toronto
+          </StyledLink>
+          <StyledDiv> to build pro-bono software. </StyledDiv>
         </div>
         <br></br>
-        {/* <div className="landing-subtitle animated animatedFadeInUp fadeInUp"> I’m currently seeking Software Engineering internships!</div> */}
-       
-      
-<a href="https://github.com/hanlinc27" target="_blank"><StyledIcon className="landing-subtitle animated animatedFadeInUp fadeInUp"src={github}></StyledIcon>
-</a>
-<a href="https://www.linkedin.com/in/hanlincheng/" target="_blank">
-<StyledIcon className="landing-subtitle animated animatedFadeInUp fadeInUp" src={linkedin}></StyledIcon>
-<CopyMailTo defaultTooltip="Copy to clipboard" copiedTooltip="Copied!"email="h6cheng@uwaterloo.ca" ><StyledEmailIcon className="landing-subtitle animated animatedFadeInUp fadeInUp"src={email}></StyledEmailIcon>
-</CopyMailTo>
-</a>
+        <div className="landing-subtitle animated animatedFadeInUp fadeInUp">
+          {' '}
+          I’m currently seeking new grad software engineering internships for 2024! I'm looking for
+          a multidisciplinary team that is kind, user-focused, and where there is room to take
+          ownership of complex engineering problems.
+        </div>
+
+        <a href="https://github.com/hanlinc27" target="_blank">
+          <StyledIcon
+            className="landing-subtitle animated animatedFadeInUp fadeInUp"
+            src={github}
+          ></StyledIcon>
+        </a>
+        <a href="https://www.linkedin.com/in/hanlincheng/" target="_blank">
+          <StyledIcon
+            className="landing-subtitle animated animatedFadeInUp fadeInUp"
+            src={linkedin}
+          ></StyledIcon>
+          <CopyMailTo
+            defaultTooltip="Copy to clipboard"
+            copiedTooltip="Copied!"
+            email="h6cheng@uwaterloo.ca"
+          >
+            <StyledEmailIcon
+              className="landing-subtitle animated animatedFadeInUp fadeInUp"
+              src={email}
+            ></StyledEmailIcon>
+          </CopyMailTo>
+        </a>
         <br></br>
         <br></br>
         <div className="row animated animatedFadeInUp fadeInUp">
           <StyledImg className="column-test animated" src={first}></StyledImg>
-                    <StyledImg className="column-test3 animated " src={fourth}></StyledImg>
-                    <StyledImg className=" column animated" src={second}></StyledImg>
+          <StyledImg className="column-test3 animated " src={fourth}></StyledImg>
+          <StyledImg className=" column animated" src={second}></StyledImg>
 
           <StyledImg className="column-test2 animated " src={third}></StyledImg>
         </div>
 
-        <div className="animated animatedFadeInUp fadeInUp">
-
-
+        {/* <div className="animated animatedFadeInUp fadeInUp">
           <StyledSubittle>Previously</StyledSubittle>
           <StyledProjectContainer>
-            <StyledSpecialProject title="Full Stack Developer Intern at Voiceflow"
+            <StyledSpecialProject
+              title="Full Stack Developer Intern at Voiceflow"
               body=" - Crafting collaborative conversation design experiences with a fully involved prototyping and user feedback focus."
               date="May 2021 - Aug 2021"
               stack="ReactJS (Typescript), Redux, Node.js, Express, Cypress, Mocha, MongoDB, Docker"
@@ -179,9 +207,9 @@ export default () => (
             <StyledProjectImage src={voiceflow}></StyledProjectImage>
           </StyledProjectContainer>
 
-
           <StyledProjectContainer>
-            <StyledProject title="Project Developer at UW Blueprint"
+            <StyledProject
+              title="Project Developer at UW Blueprint"
               body=" - Building an interactive map platform and dashboard to showcase the written stories of immigrant women through a symbolic pair of shoes."
               date="September 2020 - May 2021"
               stack="ReactJS (Typescript), Styled Components, Mapbox, LeafletJS, Golang, PostgreSQL, Docker"
@@ -191,7 +219,8 @@ export default () => (
           </StyledProjectContainer>
 
           <StyledProjectContainer>
-            <StyledProject title="Full Stack Developer Intern at Imagine Communications"
+            <StyledProject
+              title="Full Stack Developer Intern at Imagine Communications"
               body=" - Envisioning a seamless television playout experience through a file orchestration application for key video content workflows."
               date="September 2020 - December 2020"
               stack="AngularJS and VueJS (Typescript), Protractor, Jasmine, Karma, Cypress"
@@ -201,7 +230,8 @@ export default () => (
           </StyledProjectContainer>
 
           <StyledProjectContainer>
-            <StyledProject title="Frontend Developer at Civic Tech Toronto"
+            <StyledProject
+              title="Frontend Developer at Civic Tech Toronto"
               body=" - Empowering a civic grassroot organization driving tech-focused mentoring for underhoused Toronto youth. "
               date="February 2020 - December 2020"
               stack="ReactJS (Javascript), MaterialUI, Styled Components Figma"
@@ -209,9 +239,10 @@ export default () => (
             ></StyledProject>
             <StyledProjectImage src={ctto}></StyledProjectImage>
           </StyledProjectContainer>
-          
+
           <StyledProjectContainer>
-            <StyledProject title="Information Technology Intern at VMware (Formerly Pivotal Software) "
+            <StyledProject
+              title="Information Technology Intern at VMware (Formerly Pivotal Software) "
               body="  - Streamlining an internal inventory check-out system and automating the identity app migration process."
               date="January 2020 - April 2020"
               stack="ReactJS (Javascript), Google App Scripts, Cloud Foundry"
@@ -220,9 +251,9 @@ export default () => (
             <StyledProjectImage src={pivotal}></StyledProjectImage>
           </StyledProjectContainer>
 
-
           <StyledProjectContainer>
-            <StyledProject title="Software Development Intern at Sunnybrook Research Institute"
+            <StyledProject
+              title="Software Development Intern at Sunnybrook Research Institute"
               body="  - Enabling ultrasound treatment monitoring programs through parallelizing data computations."
               date="July 2019 - August 2019"
               stack="C++, CUDA, MATLAB"
@@ -230,13 +261,12 @@ export default () => (
             ></StyledProject>
             <StyledProjectImage src={sunnybrook}></StyledProjectImage>
           </StyledProjectContainer>
-        
-        
+
           <StyledSubittle>For Fun</StyledSubittle>
 
-
           <StyledProjectContainer>
-            <StyledSpecialProject title="All-nighters at hackathons"
+            <StyledSpecialProject
+              title="All-nighters at hackathons"
               body=" - A collection of hackathon projects from SheHacks IV, Ellehacks 2020, Delthacks 2020, and Hack the Valley 2020."
               date="Ongoing"
               stack="Too long of a tech stack to list."
@@ -246,9 +276,9 @@ export default () => (
             <StyledProjectImage src={hackathons}></StyledProjectImage>
           </StyledProjectContainer>
 
-
           <StyledProjectContainer>
-            <StyledSpecialProject title="Podcasting at for context: "
+            <StyledSpecialProject
+              title="Podcasting at for context: "
               body=" - Chatting away thoughts for a future self to look back and re-discover."
               date="Ongoing"
               link="https://medium.com/for-context"
@@ -257,10 +287,8 @@ export default () => (
             ></StyledSpecialProject>
             <StyledProjectImage src={forcontext}></StyledProjectImage>
           </StyledProjectContainer>
-        </div>
+        </div> */}
       </Intro>
-
     </Container>
-
   </>
 );
